135b26381da48dcac391d6fcf4a6ee4a9cd8e6ac
Dade Cook <dadecook@gmail.com>
2024-11-16 21:34:13 -0500

    fix: Use variable for convex auth issuer config

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,3 +1,3 @@
 export default {
-	providers: [{ domain: 'https://special-duck-86.clerk.accounts.dev', applicationID: 'convex' }],
+	providers: [{ domain: process.env.CLERK_URL, applicationID: 'convex' }],
 };
